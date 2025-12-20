2aef63975d250593da43cf1bd52476b5f91d4b4f
TNM03 <nurmuhammadtojiboyev496@gmail.com>
2025-12-20 16:18:42 +0500

    Delete languages.py

--- languages.py
+++
@@ -1,8 +0,0 @@
-TEXTS = {
-    "uz": {
-        "menu": "Bo‘limni tanlang:",
-        "btn_prob": "🎯 Ehtimollikni hisoblash",
-        "btn_max": "📈 Maksimal reyting",
-        "btn_news": "📰 So‘nggi yangiliklar"
-    }
-}
