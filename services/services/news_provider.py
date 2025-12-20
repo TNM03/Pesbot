d0b543faab82c6891c3460ba228b807044d97f84
TNM03 <nurmuhammadtojiboyev496@gmail.com>
2025-12-20 15:32:27 +0500

    Create news_provider.py

---
+++ services/services/news_provider.py
@@ -0,0 +1,6 @@
+def get_latest_news():
+    return [
+        "Dushanba kuni yangi Epic pack kutilmoqda.",
+        "Payshanba kuni gameplay update bo‘ladi.",
+        "Konami himoya mexanikasini kuchaytiradi."
+    ]
